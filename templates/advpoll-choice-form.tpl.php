d6bc245d54ef960d07dcc4835c6d51c3358ef60e
Georgios Tsotsos <tsotsos@gmail.com>
2015-03-03 12:08:42 +0200

    Choice form change - media files view modes
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

--- templates/advpoll-choice-form.tpl.php
+++ templates/advpoll-choice-form.tpl.php
@@ -7,15 +7,15 @@
  * - TODO: $nid: The nid of the advpoll. Needs to be set in form.
  * - TODO: $title: The title of the advpoll. Needs to be set in form (or grabbed from nid in preprocess).
  * - $choices: The radio buttons or checkboxes for the choices in the advpoll.
- * - $images: The choice images, with keys corresponding to $choices. These can be themed via theme_advpoll_field_image_image().
+ * - $media: The file view output. View mode: Polls choice form
  * - $write_in: The write-in field, if it is set.
  * - $message: Poll message (validation error, etc.), if set.
  * - $submit: The vote button.
  * - $hidden: Hidden variables that need to be rendered for the form to function.
  *
  * @see
- * - template_preprocess_advpoll_field_image_choice_form()
- * - theme_advpoll_field_image_image().
+ * - template_preprocess_advpoll_choice_form()
+ * - theme_advpoll_media().
  *
  * @ingroup themeable
  */
@@ -25,14 +25,13 @@
   <div class="advpoll-choice-form advpoll-field-image-choice-form">
 
     <div class="choices">
-      <?php foreach ($choices as $key => $choice) { ?>
+      <?php foreach ($choices as $key => $choice):?>
         <div class="choice choice-<?php print $key ?>">
-          <?php if (!empty($images)): ?>
-            <div class="choice-image"><?php print $images[$key]?></div>
+          <?php if (!empty($media)): ?><?php print $media[$key]?>
           <?php endif; ?>
           <div class="choice-text"><?php print $choice; ?></div>
         </div>
-      <?php } ?>
+      <?php endforeach; ?>
 
       <?php if (isset($write_in)): ?>
         <?php print $write_in; ?>
